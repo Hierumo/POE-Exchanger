6d8539da22eb37bac94c96d3c41f128edbc9c4ea
Hierumo <hierumo@users.noreply.github.com>
2019-06-30 19:35:19 +0300

    v0.01

---
+++ js/script.js
@@ -0,0 +1,27 @@
+(function trader() {
+    'use strict';
+
+    let calculate = document.body.querySelector('.calculate');
+    let clear = document.body.querySelector('.clear');
+    let result = document.body.querySelector('.result');
+    let text = document.body.querySelector('.text');
+
+    calculate.onclick = (e) => {
+
+        let jewelToChaos = document.getElementById('jewelToChaos').value;
+        let chaosToFusing = document.getElementById('chaosToFusing').value;
+        let jewelToFusing = document.getElementById('jewelToFusing').value;
+        e.preventDefault();
+        result.textContent = jewelToChaos * chaosToFusing;
+
+        let message = "Buy chaos orbs first";
+        if (jewelToChaos * chaosToFusing < jewelToFusing) {
+            message = "Buy jeweller's orbs";
+        }
+        text.textContent = message;
+    };
+    clear.onclick = (e) => {
+        result.textContent = "";
+        text.textContent = "";
+    };
+}());
